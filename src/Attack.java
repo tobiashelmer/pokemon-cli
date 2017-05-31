b3084d14d294e10f506838f7226251a9bed1815a
Tobias Helmer <t.helmer@creating-web.de>
2017-05-31 12:07:41 +0200

    initial

---
+++ src/Attack.java
@@ -0,0 +1,42 @@
+
+public class Attack {
+	
+	String attackName;
+	String attackType;
+	int pwr;
+	int pp;
+	int acc;
+	
+	
+	/**
+	 * Set Attackname
+	 */
+	void setAttackName(String attack) {
+		this.attackName = attack;
+	}
+	
+	/**
+	 * Get Attackname
+	 * @return String
+	 */
+	String getAttackName() {
+		return attackName;
+	}
+	
+	/**
+	 * Set Attacktype
+	 */
+	void setAttackType(String type) {
+		this.attackType = type;
+	}
+	
+	/**
+	 * Get Attacktype
+	 * @return String
+	 */
+	String getAttackType() {
+		return attackType;
+	}
+	
+	
+}
---
+++ src/Pokemon.java
@@ -0,0 +1,10 @@
+
+public class Pokemon {
+	
+	String pokemonName;
+	String pokemonType;
+	int HP;
+	int DEF;
+	int SPD;
+	int ATK;
+}
