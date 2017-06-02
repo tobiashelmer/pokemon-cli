cc56da9644495162bdfe5666ca87671d9b6620bf
Tobias Helmer <t.helmer@creating-web.de>
2017-06-02 15:22:37 +0200

    added STAB & Critical hit functions

--- src/main.java
+++ src/main.java
@@ -1,5 +1,9 @@
+import java.util.Random;
 
 public class main {
+	
+	public static final double STAB_MULTIPLIER = 1.5; 
+	public static final double CRIT_MULTIPLIER = 2; 
 
 	public static void main(String[] args) {
 		
@@ -10,23 +14,80 @@ public class main {
 		Pokemon Bisasam = new Pokemon("Bisasam", "Pflanze", Rankenhieb, 21, 13, 10, 10);
 		Pokemon Glumanda = new Pokemon("Glumanda", "Feuer", Glut, 21, 13, 10, 10);
 		
-		System.out.println("Vor dem Angriff: " + Glumanda.getHealthPoints());
-
-		System.out.println("Angriff mit " + Rankenhieb.getAttackName());
+		renderHealthBar(Glumanda);
+		
 		calculateDamage(Rankenhieb, Bisasam, Glumanda);
-		
-		System.out.println("Nach dem Angriff: " + Glumanda.getHealthPoints());
-		
+				
 	}	
 	
 	public static int calculateDamage(Attack attack, Pokemon attacker, Pokemon defender) {
 		
+		boolean stab = isStab(attack, attacker);
+		boolean crit = isCriticalHit();
+		double multiplier = 1;
+		
 		int hp = defender.getHealthPoints();
-		int damage = ((((2*5)/5)+2) * attack.getAttackPower() * (attacker.getAttack() / defender.getDefense()) / 50) + 2;
+		int attackPower = attack.getAttackPower();
+		int attackerAttack = attacker.getAttack();
+		int defenderDefense = defender.getDefense();
+		
+		if (stab) {
+			multiplier *= STAB_MULTIPLIER;
+		}
+		
+		if (crit) {
+			multiplier *= CRIT_MULTIPLIER;
+		}
+		
+		int damage = (int) ((((((2*5)/5)+2) * attackPower * (attackerAttack / defenderDefense) / 50) + 2) * multiplier);
 
 		defender.setHealthPoints(hp - damage);
+		
+		System.out.println(attack.getAttackName() + " hat " + damage + " Schaden zugefuegt!");
 		 
 		return damage;
 	}
 	
+	public static boolean isStab(Attack attack, Pokemon pokemon) {
+		
+		boolean stab = false;
+		String attackType = attack.getAttackType();
+		String pokemonType = pokemon.getType();
+		
+		if (attackType.equals(pokemonType)) {
+			stab = true;
+		}
+		
+		return stab;
+		
+	}
+	
+	public static boolean isCriticalHit() {
+				
+		boolean criticalHit = false;
+		Random random = new Random();
+	    double chance = random.nextInt(100);
+	    
+	    if (chance < 6.25) { // 6.25%
+	    	criticalHit = true;
+	    	System.out.println("Ein Volltreffer!");
+	    }
+
+	    return criticalHit;
+	}
+	
+	public static void renderHealthBar(Pokemon current) {
+		
+		int currentHP = current.getHealthPoints();
+		int maxHP = current.getMaxHealthPoints();
+		
+		String healthBarFirst  = "HP: xxxxxxxxxx ||";
+		String healthBarSecond = "       " + currentHP + " / " + maxHP + " ||";
+		String healthBarThird  = "<==============||";
+		
+		System.out.println(healthBarFirst);
+		System.out.println(healthBarSecond);
+		System.out.println(healthBarThird);
+
+	}
 }
