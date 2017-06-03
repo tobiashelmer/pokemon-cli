e46fd794ade86ce767ea6fdadff50d58952379f7
Tobias Helmer <t.helmer@creating-web.de>
2017-06-03 15:11:14 +0200

    formatting and private stuff

--- src/Move.java
+++ src/Move.java
@@ -1,195 +1,206 @@
 import java.util.Random;
 
 public class Move {
-	
-	final static double STAB_MULTIPLIER = 1.5; 
-	final static double CRIT_MULTIPLIER = 2; 
-	
-	private String moveName;
-	private String moveType;
-	private int movePower;
-	private int movePowerPoints;
-	private int moveAccuracy;
-	
-	/**
-	 * Class Constructor
-	 * @param name as String
-	 * @param type as String
-	 * @param power as int
-	 * @param powerpoints as int
-	 * @param accuracy as int
-	 */
-	public Move(String name, String type, int power, int powerpoints, int accuracy) {
-		this.moveName = name;
-		this.moveType = type;
-		this.movePower = power;
-		this.movePowerPoints = powerpoints;
-		this.moveAccuracy = accuracy;
-	}
-	
-	/**
-	 * Get the name of move 
-	 * @return String
-	 */
-	public String getmoveName() {
-		return moveName;
-	}
-	
-	/**
-	 * Set the name of move
-	 * @param move as String 
-	 */
-	public void setmoveName(String move) {
-		this.moveName = move;
-	}
-	
-	/**
-	 * Get the type of move 
-	 * @return String
-	 */
-	public String getmoveType() {
-		return moveType;
-	}
-	
-	/**
-	 * Set the type of move 
-	 * @param type as String
-	 */
-	public void setmoveType(String type) {
-		this.moveType = type;
-	}
-	
-	/**
-	 * Get the power of move 
-	 * @return int
-	 */
-	public int getmovePower() {
-		return movePower;
-	}
-	
-	/**
-	 * Set the power of move
-	 * @param power as int 
-	 */
-	public void setmovePower(int power) {
-		this.movePower = power;
-	}
-	
-	/**
-	 * Get the power points of move 
-	 * @return int
-	 */
-	public int getmovePowerPoints() {
-		return movePowerPoints;
-	}
-	
-	/**
-	 * Set the power points of move
-	 * @param powerpoints as int 
-	 */
-	public void setmovePowerPoints(int powerpoints) {
-		this.movePowerPoints = powerpoints;
-	}
-	
-	/**
-	 * Get the accuracy of move 
-	 * @return int
-	 */
-	public int getmoveAccuracy() {
-		return moveAccuracy;
-	}
-	
-	/**
-	 * Set the accuracy of move
-	 * @param accuracy as int 
-	 */
-	public void setmoveAccuracy(int accuracy) {
-		this.moveAccuracy = accuracy;
-	}
-	
-	/**
-	 * Calculates the damage done by the moveer pokemon and returns it as int
-	 * @param move as move
-	 * @param moveer as Pokemon
-	 * @param defender as Pokemon
-	 * @return int
-	 */
-	public static int calculateDamage(Move move, Pokemon attacker, Pokemon defender) {
-		
-		boolean stab = isStab(move, attacker);
-		boolean crit = isCriticalHit();
-		double multiplier = 1;
-		int damage;
-		int hp = defender.getHealthPoints();
-		int movePower = move.getmovePower();
-		int attackerAttack = attacker.getAttack();
-		int defenderDefense = defender.getDefense();
-		
-		// if type matches (see isStab()) adds STAB_MULTIPLIER to multiplier
-		if (stab) {
-			multiplier *= STAB_MULTIPLIER;
-		}
-		
-		// if move crits (see isCrit()) adds CRIT_MULTIPLIER to multiplier
-		if (crit) {
-			multiplier *= CRIT_MULTIPLIER;
-		}
-		
-		// damage calculation
-		damage = (int) ((((((2*5)/5)+2) * movePower * (attackerAttack / defenderDefense) / 50) + 2) * multiplier);
 
-		// lowers Pokemon HP by the damage dealt
-		defender.setHealthPoints(hp - damage);
-		
-		// prints out damage message
-		System.out.println(move.getmoveName() + " hat " + damage + " Schaden zugefuegt!");
-		 
-		return damage;
-	}
-	
-	
-	/**
-	 * Same Type move Bonus (STAB)
-	 * <br>
-	 * 50% damage boost of an move when it is the same type as one of the types of the Pokemon using the move
-	 * 
-	 * @param move as move
-	 * @param pokemon as Pokemon
-	 * @return boolean
-	 */
-	public static boolean isStab(Move move, Pokemon pokemon) {
-		
-		boolean stab = false;
-		String moveType = move.getmoveType();
-		String pokemonType = pokemon.getType();
-		
-		// if move type matches pokemon type set stab to true
-		if (moveType.equals(pokemonType)) {
-			stab = true;
-		}
-		
-		return stab;
-		
-	}
-	
-	/**
-	 * Random event that doubles the damage of a damage-dealing move. 
-	 * @return boolean
-	 */
-	public static boolean isCriticalHit() {
-				
-		boolean criticalHit = false;
-		Random random = new Random();
-	    double chance = random.nextInt(100);
-	    
-	    // simulates a 6.25% chance of a critical hit
-	    if (chance < 6.25) {
-	    	criticalHit = true;
-	    	System.out.println("Ein Volltreffer!");
-	    }
+    private final static double STAB_MULTIPLIER = 1.5;
+    private final static double CRIT_MULTIPLIER = 2;
 
-	    return criticalHit;
-	}
-	
-	
+    private String moveName;
+    private String moveType;
+    private int movePower;
+    private int movePowerPoints;
+    private int moveAccuracy;
+
+    /**
+     * Class Constructor
+     *
+     * @param name        as String
+     * @param type        as String
+     * @param power       as int
+     * @param powerpoints as int
+     * @param accuracy    as int
+     */
+    public Move(String name, String type, int power, int powerpoints, int accuracy) {
+        this.moveName = name;
+        this.moveType = type;
+        this.movePower = power;
+        this.movePowerPoints = powerpoints;
+        this.moveAccuracy = accuracy;
+    }
+
+    /**
+     * Get the name of move
+     *
+     * @return String
+     */
+    public String getmoveName() {
+        return moveName;
+    }
+
+    /**
+     * Set the name of move
+     *
+     * @param move as String
+     */
+    public void setmoveName(String move) {
+        this.moveName = move;
+    }
+
+    /**
+     * Get the type of move
+     *
+     * @return String
+     */
+    public String getmoveType() {
+        return moveType;
+    }
+
+    /**
+     * Set the type of move
+     *
+     * @param type as String
+     */
+    public void setmoveType(String type) {
+        this.moveType = type;
+    }
+
+    /**
+     * Get the power of move
+     *
+     * @return int
+     */
+    public int getmovePower() {
+        return movePower;
+    }
+
+    /**
+     * Set the power of move
+     *
+     * @param power as int
+     */
+    public void setmovePower(int power) {
+        this.movePower = power;
+    }
+
+    /**
+     * Get the power points of move
+     *
+     * @return int
+     */
+    public int getmovePowerPoints() {
+        return movePowerPoints;
+    }
+
+    /**
+     * Set the power points of move
+     *
+     * @param powerpoints as int
+     */
+    public void setmovePowerPoints(int powerpoints) {
+        this.movePowerPoints = powerpoints;
+    }
+
+    /**
+     * Get the accuracy of move
+     *
+     * @return int
+     */
+    public int getmoveAccuracy() {
+        return moveAccuracy;
+    }
+
+    /**
+     * Set the accuracy of move
+     *
+     * @param accuracy as int
+     */
+    public void setmoveAccuracy(int accuracy) {
+        this.moveAccuracy = accuracy;
+    }
+
+    /**
+     * Calculates the damage done by the moveer pokemon and returns it as int
+     *
+     * @param move     as move
+     * @param attacker as Pokemon
+     * @param defender as Pokemon
+     * @return int
+     */
+    public static int calculateDamage(Move move, Pokemon attacker, Pokemon defender) {
+
+        boolean stab = isStab(move, attacker);
+        boolean crit = isCriticalHit();
+        double multiplier = 1;
+        int damage;
+        int hp = defender.getHealthPoints();
+        int movePower = move.getmovePower();
+        int attackerAttack = attacker.getAttack();
+        int defenderDefense = defender.getDefense();
+
+        // if type matches (see isStab()) adds STAB_MULTIPLIER to multiplier
+        if (stab) {
+            multiplier *= STAB_MULTIPLIER;
+        }
+
+        // if move crits (see isCrit()) adds CRIT_MULTIPLIER to multiplier
+        if (crit) {
+            multiplier *= CRIT_MULTIPLIER;
+        }
+
+        // damage calculation
+        damage = (int) ((((((2 * 50) / 5) + 2) * movePower * (attackerAttack / defenderDefense) / 50) + 2) * multiplier);
+
+        // lowers Pokemon HP by the damage dealt
+        defender.setHealthPoints(hp - damage);
+
+        // prints out damage message
+        System.out.println(move.getmoveName() + " hat " + damage + " Schaden zugefuegt!");
+
+        return damage;
+    }
+
+
+    /**
+     * Same Type move Bonus (STAB)
+     * <br>
+     * 50% damage boost of an move when it is the same type as one of the types of the Pokemon using the move
+     *
+     * @param move    as move
+     * @param pokemon as Pokemon
+     * @return boolean
+     */
+    private static boolean isStab(Move move, Pokemon pokemon) {
+
+        boolean stab = false;
+        String moveType = move.getmoveType();
+        String pokemonType = pokemon.getType();
+
+        // if move type matches pokemon type set stab to true
+        if (moveType.equals(pokemonType)) {
+            stab = true;
+        }
+
+        return stab;
+
+    }
+
+    /**
+     * Random event that doubles the damage of a damage-dealing move.
+     *
+     * @return boolean
+     */
+    private static boolean isCriticalHit() {
+
+        boolean criticalHit = false;
+        Random random = new Random();
+        double chance = random.nextInt(100);
+
+        // simulates a 6.25% chance of a critical hit
+        if (chance < 6.25) {
+            criticalHit = true;
+            System.out.println("Ein Volltreffer!");
+        }
+
+        return criticalHit;
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,35 +1,67 @@
 public class main {
-	
-	public static void main(String[] args) {
-		
-		Move tackle = new Move("Tackle", "Normal", 20, 20, 100);
-		Move rankenhieb = new Move("Rankenhieb", "Pflanze", 35, 10, 100);
-		Move glut = new Move("Glut", "Feuer", 40, 25, 100);
 
-		Pokemon bisasam = new Pokemon("Bisasam", "Pflanze", rankenhieb, 21, 13, 10, 10);
-		Pokemon glumanda = new Pokemon("Glumanda", "Feuer", glut, 21, 13, 10, 10);
-		
-		//renderHealthBar(Glumanda);
-		
-		Move.calculateDamage(rankenhieb, bisasam, glumanda);				
-	}	
-	
-	public static void choosePokemon() {
-		
-	}
-			
-	public static void renderHealthBar(Pokemon current) {
-		
-		int currentHP = current.getHealthPoints();
-		int maxHP = current.getMaxHealthPoints();
-		
-		String healthBarFirst  = "HP: xxxxxxxxxx ||";
-		String healthBarSecond = "       " + currentHP + " / " + maxHP + " ||";
-		String healthBarThird  = "<==============||";
-		
-		System.out.println(healthBarFirst);
-		System.out.println(healthBarSecond);
-		System.out.println(healthBarThird);
+    public static void main(String[] args) {
 
-	}
+        // normal moves
+        Move tackle = new Move("Tackle", "Normal", 50, 35, 100);
+        Move ruckzuckhieb = new Move("Ruckzuckhieb", "Normal", 40, 30, 100);
+
+        // grass moves
+        Move rankenhieb = new Move("Rankenhieb", "Pflanze", 35, 10, 100);
+        Move rasierblatt = new Move("Rasierblatt", "Pflanze", 55, 25, 95);
+
+        // fire moves
+        Move glut = new Move("Glut", "Feuer", 40, 25, 100);
+        Move flammenwurf = new Move("Flammenwurf", "Feuer", 90, 15, 100);
+
+        // water moves
+        Move aquaknarre = new Move("Aquaknarre", "Wasser", 40, 25, 100);
+        Move surfer = new Move("Surfer", "Wasser", 90, 15, 100);
+
+
+        // misc moves
+        Move erdbeben = new Move("Erdbeben", "Boden", 100, 10, 100);
+        Move eisstrahl = new Move("Eisstrahl", "Eis", 90, 10, 100);
+        Move donnerblitz = new Move("Donnerblitz", "Elektro", 90, 15, 100);
+        Move psychokinese = new Move("Psychokinese", "Psycho", 90, 10, 100);
+        Move windstoss = new Move("Windstoß", "Flug", 60, 35, 100);
+        Move drachenwut = new Move("Drachenwut", "Drache", 40, 10, 100);
+        Move steinhagel = new Move("Steinhagel", "Gestein", 75, 10, 90);
+
+
+        Pokemon bisaflor = new Pokemon("Bisaflor", "Pflanze", rasierblatt, 187, 134, 135, 132);
+        Pokemon glurak = new Pokemon("Glumanda", "Feuer", flammenwurf, 185, 136, 130, 152);
+        Pokemon turtok = new Pokemon("Turtok", "Wasser", surfer, 186, 137, 152, 130);
+        Pokemon pikachu = new Pokemon("Pikachu", "Elektro", donnerblitz, 142, 107, 92, 142);
+        Pokemon tauboss = new Pokemon("Tauboss", "Flug", windstoss, 190, 132, 127, 153);
+        Pokemon garados = new Pokemon("Garados", "Wasser", surfer, 202, 177, 131, 133);
+        Pokemon simsala = new Pokemon("Simsala", "Psycho", psychokinese, 162, 187, 97, 172);
+        Pokemon gengar = new Pokemon("Gengar", "Geist", psychokinese, 167, 222, 132, 182);
+        Pokemon dragoran = new Pokemon("Dragoran", "Drache", drachenwut, 198, 186, 147, 132);
+        Pokemon relaxo = new Pokemon("Relaxo", "Normal", tackle, 267, 162, 117, 82);
+
+
+        //renderHealthBar(Glumanda);
+
+        Move.calculateDamage(psychokinese, simsala, relaxo);
+    }
+
+    public static void choosePokemon() {
+
+    }
+
+    public static void renderHealthBar(Pokemon current) {
+
+        int currentHP = current.getHealthPoints();
+        int maxHP = current.getMaxHealthPoints();
+
+        String healthBarFirst = "HP: xxxxxxxxxx ||";
+        String healthBarSecond = "       " + currentHP + " / " + maxHP + " ||";
+        String healthBarThird = "<==============||";
+
+        System.out.println(healthBarFirst);
+        System.out.println(healthBarSecond);
+        System.out.println(healthBarThird);
+
+    }
 }
