07a881cd81a2e177e42a7dd4dca6604eaefaf101
Tobias Helmer <t.helmer@creating-web.de>
2017-07-05 16:18:25 +0200

    added Game.java for every game related stuff

---
+++ src/Game.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Game {
+
+    static Scanner scannerRead = new Scanner(System.in);
+
+    static ArrayList<Pokemon> poolOfPokemon = new ArrayList<>();
+    static ArrayList<Pokemon> teamPokemon = new ArrayList<>();
+    static ArrayList<Pokemon> rivalPokemon = new ArrayList<>();
+
+    static Move rasierblatt = new Move("Rasierblatt", "Pflanze", 55, 25, 95);
+
+
+    public static void generateMoves() {
+    }
+
+    public static void generatePokemon() {
+
+        Pokemon bisaflor = new Pokemon("Bisaflor", "Pflanze", rasierblatt, 50, 80, 100, 83, 80);
+        Pokemon turtok = new Pokemon("Turtok", "Pflanze", rasierblatt, 50, 80, 100, 83, 80);
+        Pokemon glurak = new Pokemon("Glurak", "Pflanze", rasierblatt, 50, 80, 100, 83, 80);
+        Pokemon mewtu = new Pokemon("Mewtu", "Pflanze", rasierblatt, 50, 80, 100, 83, 80);
+
+        poolOfPokemon.add(bisaflor);
+        poolOfPokemon.add(turtok);
+        poolOfPokemon.add(glurak);
+        poolOfPokemon.add(mewtu);
+
+    }
+
+    public static void startGame() {
+
+        String start = "Hallo! Bitte gib dir selbst einen Namen: ";
+        System.out.print(start);
+        String name = scannerRead.nextLine();
+        System.out.println("Hallo " + name + "!");
+
+        generatePokemon();
+        selectPokemon();
+        rivalPokemon();
+    }
+
+    public static void selectPokemon() {
+
+        String selectPokemon = "Bitte wähle nun drei Pokemon für dein Team aus.";
+        System.out.println(selectPokemon);
+
+        do {
+
+            for (int i = 0; i < poolOfPokemon.size(); i++) {
+                Pokemon arrayListPokemon = poolOfPokemon.get(i);
+                System.out.println("(" + (i + 1) + ") " + arrayListPokemon.getName());
+            }
+
+            try {
+                System.out.print("Auswahl des ersten Pokemons: ");
+                int pokemonNumber = scannerRead.nextInt();
+                teamPokemon.add(poolOfPokemon.get((pokemonNumber - 1)));
+                poolOfPokemon.remove((pokemonNumber - 1));
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Bitte wählen Sie ein Pokemon aus der Liste aus.");
+            }
+
+        } while (teamPokemon.size() < 3);
+
+        for(Pokemon poke:teamPokemon) {
+            System.out.print(poke.getName());
+        }
+
+    }
+
+    public static void rivalPokemon() {
+
+        while(rivalPokemon.size() < 3) {
+            try {
+                int pokemonNumber = (int) (Math.random() * poolOfPokemon.size() + 1);
+                teamPokemon.add(poolOfPokemon.get((pokemonNumber - 1)));
+                poolOfPokemon.remove((pokemonNumber - 1));
+            } catch (IndexOutOfBoundsException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+}
---
+++ src/Player.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class Player {
+
+    private String name;
+    ArrayList<Pokemon> team = new ArrayList<>();
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get Trainer Name
+     *
+     * @return String
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set Trainer name
+     *
+     * @param name as String
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get Team
+     *
+     * @return team as ArrayList
+     */
+    public ArrayList getTeam() {
+        return team;
+    }
+
+    /**
+     * Set Team
+     *
+     * @param pokemon as Pokemon
+     */
+
+    public void setTeam(Pokemon pokemon) {
+        this.team.add(pokemon);
+    }
+
+
+
+}
