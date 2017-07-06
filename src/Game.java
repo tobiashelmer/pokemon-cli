e550d4308edeb2cae70f30ed22bbb0056c4c0aed
Tobias Helmer <t.helmer@creating-web.de>
2017-07-06 16:03:57 +0200

    added static TEAM_COUNT variable added private to methods added some testpokemon moved prints to String variables added QoL changes for Strings

--- src/Game.java
+++ src/Game.java
@@ -3,33 +3,43 @@ import java.util.Scanner;
 
 public class Game {
 
-    static Scanner scannerRead = new Scanner(System.in);
+    private final static int TEAM_COUNT = 3;
 
-    static ArrayList<Pokemon> poolOfPokemon = new ArrayList<>();
-    static ArrayList<Pokemon> teamPokemon = new ArrayList<>();
-    static ArrayList<Pokemon> rivalPokemon = new ArrayList<>();
+    private static Scanner scannerRead = new Scanner(System.in);
 
-    static Move rasierblatt = new Move("Rasierblatt", "Pflanze", 55, 25, 95);
+    private static ArrayList<Pokemon> poolOfPokemon = new ArrayList<>();
+    private static ArrayList<Pokemon> teamPokemon = new ArrayList<>();
+    private static ArrayList<Pokemon> rivalPokemon = new ArrayList<>();
+
+    private static Move rasierblatt = new Move("Rasierblatt", "Pflanze", 55, 25, 95);
 
 
     public static void generateMoves() {
     }
 
-    public static void generatePokemon() {
+    private static void generatePokemon() {
 
         Pokemon bisaflor = new Pokemon("Bisaflor", "Pflanze", rasierblatt, 50, 80, 100, 83, 80);
         Pokemon turtok = new Pokemon("Turtok", "Pflanze", rasierblatt, 50, 80, 100, 83, 80);
         Pokemon glurak = new Pokemon("Glurak", "Pflanze", rasierblatt, 50, 80, 100, 83, 80);
         Pokemon mewtu = new Pokemon("Mewtu", "Pflanze", rasierblatt, 50, 80, 100, 83, 80);
+        Pokemon bisaflor2 = new Pokemon("Bisaflor2", "Pflanze", rasierblatt, 50, 80, 100, 83, 80);
+        Pokemon turtok2 = new Pokemon("Turtok2", "Pflanze", rasierblatt, 50, 80, 100, 83, 80);
+        Pokemon glurak2 = new Pokemon("Glurak2", "Pflanze", rasierblatt, 50, 80, 100, 83, 80);
+        Pokemon mewtu2 = new Pokemon("Mewtu2", "Pflanze", rasierblatt, 50, 80, 100, 83, 80);
 
         poolOfPokemon.add(bisaflor);
         poolOfPokemon.add(turtok);
         poolOfPokemon.add(glurak);
         poolOfPokemon.add(mewtu);
+        poolOfPokemon.add(bisaflor2);
+        poolOfPokemon.add(turtok2);
+        poolOfPokemon.add(glurak2);
+        poolOfPokemon.add(mewtu2);
 
     }
 
-    public static void startGame() {
+    static void startGame() {
 
         String start = "Hallo! Bitte gib dir selbst einen Namen: ";
         System.out.print(start);
@@ -41,19 +51,29 @@ public class Game {
         rivalPokemon();
     }
 
-    public static void selectPokemon() {
+    private static void selectPokemon() {
 
-        String selectPokemon = "Bitte wähle nun drei Pokemon für dein Team aus.";
-        System.out.println(selectPokemon);
+        String selectPokemonText = "Bitte wähle nun drei Pokemon für dein Team aus.";
+        System.out.println(selectPokemonText);
 
         do {
 
-            for (Pokemon poke:poolOfPokemon) {
-                System.out.println("("+(poolOfPokemon.indexOf(poke) + 1)+") " + poke.getName());
+            for (Pokemon pokemon : poolOfPokemon) {
+                System.out.println("(" + (poolOfPokemon.indexOf(pokemon) + 1) + ") " + pokemon.getName());
+            }
+
+            String pickPokemonNumberText;
+            if (teamPokemon.size() == 0) {
+                pickPokemonNumberText = "ersten";
+            } else if (teamPokemon.size() == 1) {
+                pickPokemonNumberText = "zweiten";
+            } else {
+                pickPokemonNumberText = "dritten";
             }
 
             try {
-                System.out.print("Auswahl des ersten Pokemons: ");
+                String pickPokemonText = "Auswahl des " + pickPokemonNumberText + " Pokemons: ";
+                System.out.print(pickPokemonText);
                 int pokemonNumber = scannerRead.nextInt();
                 teamPokemon.add(poolOfPokemon.get((pokemonNumber - 1)));
                 poolOfPokemon.remove((pokemonNumber - 1));
@@ -61,27 +81,35 @@ public class Game {
                 System.out.println("Bitte wählen Sie ein Pokemon aus der Liste aus.");
             }
 
-        } while (teamPokemon.size() < 3);
+        } while (teamPokemon.size() < TEAM_COUNT);
 
-        for(Pokemon poke:teamPokemon) {
-            System.out.print(poke.getName());
+        String yourPokemonTeamText = "Gewählte Pokemon: ";
+        System.out.print(yourPokemonTeamText);
+
+        for (Pokemon pokemon : teamPokemon) {
+            String yourPokemonTeamSeperatorText;
+
+            if (teamPokemon.indexOf(pokemon) != (TEAM_COUNT - 1)) {
+                yourPokemonTeamSeperatorText = ", ";
+            } else {
+                yourPokemonTeamSeperatorText = "";
+            }
+            System.out.print(pokemon.getName() + yourPokemonTeamSeperatorText);
         }
-
     }
 
-    public static void rivalPokemon() {
+    private static void rivalPokemon() {
 
-        while(rivalPokemon.size() < 3) {
+        while (rivalPokemon.size() < TEAM_COUNT) {
+
             try {
-                int pokemonNumber = (int) (Math.random() * poolOfPokemon.size() + 1);
-                teamPokemon.add(poolOfPokemon.get((pokemonNumber - 1)));
-                poolOfPokemon.remove((pokemonNumber - 1));
+                int pokemonNumber = (int) (Math.random() * poolOfPokemon.size());
+                rivalPokemon.add(poolOfPokemon.get(pokemonNumber));
+                poolOfPokemon.remove(pokemonNumber);
             } catch (IndexOutOfBoundsException e) {
                 e.printStackTrace();
             }
         }
-
-
     }
 
 }
